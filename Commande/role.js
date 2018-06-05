2e39ca6b41f1246ef90296dc6cfdddaafbdaac1f
Alexlcr <34378196+Alexlcr@users.noreply.github.com>
2018-06-05 23:51:42 +0200

    Update role.js

--- Commande/role.js
+++ Commande/role.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js')
-const Command = require('./Commande/command')
-
+const Command = require('./command')
 module.exports = class Role extends Command {
 
 	static match(message) {
@@ -64,7 +63,7 @@ module.exports = class Role extends Command {
 	
 			var embed = new Discord.RichEmbed()
 			.setColor("c11111")
-			.addField("V'la la voitu", "Vous avez accès à tous les channels concernant ce Youtubeur")
+			.addField(":musical_score: Bienvenue dans la communauté des amateurs de son !", "Vous avez accès à tous les channels concernant ce Youtubeur")
 			message.channel.send(embed)
 			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/channel/UCow2IGnug1l3Xazkrc5jM_Q')
 	
@@ -74,7 +73,7 @@ module.exports = class Role extends Command {
 	
 			var embed = new Discord.RichEmbed()
 			.setColor("c11111")
-			.addField("V'la la voiture qui t'emmène bans la commu de Mastu", "Vous avez accès à tous les channels le concernant")
+			.addField("V'la la voiture qui t'emmène dans la commu de Mastu", "Vous avez accès à tous les channels le concernant")
 			message.channel.send(embed)
 			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/channel/UCAhaFPP6v3WCfK5Tjao0B7A')
 
@@ -84,8 +83,98 @@ module.exports = class Role extends Command {
 	
 			var embed = new Discord.RichEmbed()
 			.setColor("c11111")
-			.addField("", "Vous avez accès à tous les channels le concernant")
+			.addField("______", "Vous avez accès à tous les channels le concernant")
 			message.channel.send(embed)
-			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/user/VodKprod')
+			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/channel/UCTt2AnK--mnRmICnf-CCcrw')
+
+		}else if (message.content.startsWith('/DrNozman')) {
+	
+			message.member.addRole(message.member.guild.roles.find('name','Communauté Dr Nozman'));
+	
+			var embed = new Discord.RichEmbed()
+			.setColor("c11111")
+			.addField("Non d'un neutrinion !! T'as rejoins la communauté de Dr Nozman!! :microscope: ", "Vous avez accès à tous les channels le concernant")
+			message.channel.send(embed)
+			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/user/DrNozman')
+
+		}else if (message.content.startsWith('/GearLocker')) {
+	
+			message.member.addRole(message.member.guild.roles.find('name','Communauté Gear Locker'));
+	
+			var embed = new Discord.RichEmbed()
+			.setColor("c11111")
+			.addField("Réglez vos hop-up et direction le terrain d'airsoft !! ", "Vous avez accès à tous les channels le concernant")
+			message.channel.send(embed)
+			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/channel/UC4B3MCi-egoxfKd73j66rlw')
+
+		}else if (message.content.startsWith('/JoueurDuGrenier')) {
+	
+			message.member.addRole(message.member.guild.roles.find('name','Communauté Joueur du Grenier'));
+	
+			var embed = new Discord.RichEmbed()
+			.setColor("c11111")
+			.addField("Dépoussiérez vos cassettes de jeux ici elles serviront ! :video_game: ", "Vous avez accès à tous les channels le concernant")
+			message.channel.send(embed)
+			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/user/joueurdugrenier')
+
+		}else if (message.content.startsWith('/Mamytwink')) {
+	
+			message.member.addRole(message.member.guild.roles.find('name','Communauté Mamytwink'));
+	
+			var embed = new Discord.RichEmbed()
+			.setColor("c11111")
+			.addField("Prenez votre lampe on part dans un lieu abandonné !:flashlight: ", "Vous avez accès à tous les channels le concernant")
+			message.channel.send(embed)
+			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/channel/UCjxfoYq1vAEGU7KD_C3I48g')
+
+		}else if (message.content.startsWith('/Cyprien')) {
+	
+			message.member.addRole(message.member.guild.roles.find('name','Communauté Cyprien'));
+	
+			var embed = new Discord.RichEmbed()
+			.setColor("c11111")
+			.addField("POURQUOI t'as rejoins cette communauté ?!", "Vous avez accès à tous les channels le concernant")
+			message.channel.send(embed)
+			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/channel/UCyWqModMQlbIo8274Wh_ZsQ')
+
+		}else if (message.content.startsWith('/100%Bitume')) {
+	
+			message.member.addRole(message.member.guild.roles.find('name','Communauté 100% BITUME'));
+	
+			var embed = new Discord.RichEmbed()
+			.setColor("c11111")
+			.addField("Enfourche ta motobilette tu pars dans la commu de 100%Bitume", "Vous avez accès à tous les channels le concernant")
+			message.channel.send(embed)
+			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/user/100poursangBITUME')
+
+		}else if (message.content.startsWith('/FabienOlicard')) {
+	
+			message.member.addRole(message.member.guild.roles.find('name','Communauté FabienOlicard'));
+	
+			var embed = new Discord.RichEmbed()
+			.setColor("c11111")
+			.addField("Laisse moi deviner... Tu es abonné(e) à la chaine de Fabien Olicard ! Je sais, c'est impressionnant... ", "Vous avez accès à tous les channels le concernant")
+			message.channel.send(embed)
+			message.reply('Voici le lien de sa chaine YouTube:       https://www.youtube.com/user/FabienOlicard')
+
+		}else if (message.content.startsWith('/CYR!L')) {
+	
+			message.member.addRole(message.member.guild.roles.find('name','Communauté CYR!L'));
+	
+			var embed = new Discord.RichEmbed()
+			.setColor("c11111")
+			.addField("Viens avec nous dans la quête du saint couteau ! :dagger:  :crown: ", "Vous avez accès à tous les channels le concernant")
+			message.channel.send(embed)
+			message.reply('Voici le lien de sa chaine YouTube:      https://www.youtube.com/channel/UC-4M8AN08hw39nn2v91VuMQ')
+
+		}else if (message.content.startsWith('/Bapt&Gael')) {
+	
+			message.member.addRole(message.member.guild.roles.find('name','Communauté Bapt&Gael'));
+	
+			var embed = new Discord.RichEmbed()
+			.setColor("c11111")
+			.addField("_____________", "Vous avez accès à tous les channels le concernant")
+			message.channel.send(embed)
+			message.reply('Voici le lien de sa chaine YouTube:      https://www.youtube.com/channel/UCcyGqHmijBxtOcvvIAHqJrQ')
 		}
 		}}
